e9ee75e0cfca066901038158cbbe2a2c5776e4f4
keybladesonic <keybladesonic@users.noreply.github.com>
2017-09-21 17:34:24 -0700

    updated parser.h with changes from before class
    has all the changes, needs to have changes to the functions/ finish all the functions;
    
    need to figure out how to change how the tokens/lexemes are consumed to move forward within each function call

--- parser.h
+++ parser.h
@@ -3,10 +3,115 @@
 
 #include "lexer.h"
 
-void parser(ifstream file){
+
+
+void parser(ifstream &file){
+	string getToken();
+	LexTok curToken = lexer(file);
+
+	bool accept(string s){
+		if (s == curToken){
+			curToken = getToken();
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+
+
+
+	void program(){
+		expect("program");
+		
+		if (curToken.token == "Type"){
+		
+			DeclList();
+
+		}
+		else if(curToken.lexeme == "function"){
+		
+			FuncList();
+		}
+
+
+		expect("begin");
+		
+		if (curToken.lexeme != "end"){
+			
+			StmtList();
+			
+		}
+		expect(".");
+
+		cout << "Program => program [DeclList] [FuncList] begin [StmtList] end." << endl;
+	}
+
+	void DeclList(){
+		
+		
+		do{
+		
+			Decl();
+		} while (curToken.token == "Type");
+
+
+		cout << "DeclList => Decl {Decl}" << endl;
+	}
+
+	void Decl(){
 	
+		Type();
+		VarList();
 
+		expect(";");
+		cout << "Decl => Type VarList ;" << endl;
+	}
 
+	void FuncList(){
+		
+		do{
+			Func();
+		} while (curToken.lexeme == "function");
+
+		cout << "FuncList =>Func {Func}" << endl;
+	}
+	void Func(){
+		expect("function");
+		Ident();
+
+		expect("(");
+
+		if (curToken.lexeme != ")"){
+			ParamList();
+		}
+
+		expect(")");
+
+		expect(":");
+		Type();
+		expect(";");
+
+		if (curToken.lexeme != "begin"){
+			DeclList();
+		}
+		expect("begin");
+		if (curToken.lexeme != "end"){
+			StmtList();
+
+		}
+		expect("end");
+	
+		cout << "function Ident ( [ParamList] ): Type ; [DeclList] begin [StmtList] end " << endl;
+	}
+	void ParamList(){
+	
+	
+	
+	}
+
+	void Ident(){}
 }
 
 #endif
