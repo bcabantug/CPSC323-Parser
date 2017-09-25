1fe7522446734e60557fb5799fc00d15c2c9d5e6
gracederderian <grace.derderian@csu.fullerton.edu>
2017-09-25 14:21:23 -0700

    Upload parser.h

--- parser.h
+++ parser.h
@@ -4,12 +4,10 @@
 #include "lexer.h"
 
 //Function Prototypes
-
-bool accept(string, LexTok&, ifstream&);
-void expect(string, LexTok&, ifstream&);
-void Program(ifstream&, LexTok&);
-void DeclList(ifstream&, LexTok&);
-
+bool accept(string, LexTok, ifstream&);
+void expect(string, LexTok, ifstream&);
+void program();
+void DeclList();
 void Decl();
 void FuncList();
 void Func();
@@ -37,21 +35,19 @@ void ArgList();
 
 
 void parser(ifstream &file) {
-	LexTok& curToken = lexer(file);
+	LexTok curToken = lexer(file);
 
 	
-	Program(file, curToken);
+
 
 
 }
 
-//lexeme check
-bool accept(string s, LexTok& check, ifstream &file) {
+bool accept(string s, LexTok check, ifstream &file) {
 
-	//checks the string with the lexeme
-	if (s.compare(check.lexeme) == 0) {
+	//checks thge 
+	if (s.compare(check.token) == 0) {
 		check = lexer(file);
-		cout << "yay" << endl;
 		return true;
 	}
 	else {
@@ -59,10 +55,9 @@ bool accept(string s, LexTok& check, ifstream &file) {
 	}
 }
 
-void expect(string s, LexTok& check, ifstream &file) {
+void expect(string s, LexTok check, ifstream &file) {
 	if (!accept(s, check, file)) {
 		//writeError()
-		cout << "ERROR" << endl;
 	}
 
 }
@@ -70,182 +65,180 @@ void expect(string s, LexTok& check, ifstream &file) {
 
 
 
-void Program(ifstream& file, LexTok& token) {
-	expect("program", token, file);
+void program() {
+	expect("program");
 
-	if (token.token.compare("Type") == 0) {
+	if (curToken.token == "Type") {
 
-		DeclList(file, token);
+		DeclList();
 
 	}
-	//else if (curToken.lexeme == "function") {
+	else if (curToken.lexeme == "function") {
 
-	//	FuncList();
-	//}
+		FuncList();
+	}
 
 
-	//expect("begin");
+	expect("begin");
 
-	//if (curToken.lexeme != "end") {
+	if (curToken.lexeme != "end") {
 
-	//	StmtList();
+		StmtList();
 
-	//}
-	//expect(".");
+	}
+	expect(".");
 
 	cout << "Program => program [DeclList] [FuncList] begin [StmtList] end." << endl;
 }
 
-void DeclList(ifstream& file, LexTok& current) {
+void DeclList() {
 
 
 	do {
 
-		//Decl();
-
-	} while (current.token == "Type");
+		Decl();
+	} while (curToken.token == "Type");
 
 
 	cout << "DeclList => Decl {Decl}" << endl;
 }
 
-//void Decl() {
-//
-//	Type();
-//	VarList();
-//
-//	expect(";");
-//	cout << "Decl => Type VarList ;" << endl;
-//}
+void Decl() {
 
-//
-//void FuncList() {
-//
-//	do {
-//		Func();
-//	} while (curToken.lexeme == "function");
-//
-//	cout << "FuncList =>Func {Func}" << endl;
-//}
-//void Func() {
-//	expect("function");
-//	Ident();
-//
-//	expect("(");
-//
-//	if (curToken.lexeme != ")") {
-//		ParamList();
-//	}
-//
-//	expect(")");
-//
-//	expect(":");
-//	Type();
-//	expect(";");
-//
-//	if (curToken.lexeme != "begin") {
-//		DeclList();
-//	}
-//	expect("begin");
-//	if (curToken.lexeme != "end") {
-//		StmtList();
-//
-//	}
-//	expect("end");
-//
-//	cout << "Func => function Ident ( [ParamList] ): Type ; [DeclList] begin [StmtList] end " << endl;
-//}
-//void ParamList() {
-//
-//
-//	cout << "ParamList => Param {, Param}" << endl;
-//}
-//
-//void Param() {
-//
-//	cout << "Param => Type Ident" << endl;
-//}
-//void Type() {
-//
-//	cout << "Type => int | real | string" << endl;
-//}
-//
-//void VarList() {
-//
-//	cout << "VarList => Ident {,Ident}" << endl;
-//}
-//
-//void StmtList() {
-//
-//	cout << "StmtList => Stmt{Stmt}" << endl;
-//}
-//
-//void Stmt() {
-//
-//	cout << "Stmt => Assign | Read | Write | If | While | Do | Return" << endl;
-//}
-//void Assign() {
-//
-//	cout << "Assign => Ident := Expr;";
-//}
-//void Read() {
-//
-//	cout << "Read => read ( VarList ) ; " << endl;
-//}
-//
-//void Write() {
-//
-//
-//	cout << "Write => write ( Expr {, Expr} ) ;" << endl;
-//
-//}
-//
-//void If() {
-//
-//	cout << "If => if ( Cond ) begin StmtList end { elsif ( Cond ) begin StmtList end } [else begin StmtList end ]" << endl;
-//}
-//void While() {
-//
-//
-//
-//	cout << "While => while ( Cond ) begin [StmtList] end" << endl;
-//}
-//
-//void Do() {
-//	cout << "do => do [StmtList] until ( Cond ) ;" << endl;
-//}
-//
-//void Return() {
-//
-//	cout << "Return => return Expr ;" << endl;
-//}
-//
-//void Cond() {
-//	cout << "Cond => Expr RelOp Expr" << endl;
-//}
-//void RelOp() {
-//	cout << "RelOp => > | < | >= | <= | = | <> " << endl;
-//}
-//void Expr() {
-//	cout << "Expr => Term { (+|-) Term }" << endl;
-//}
-//void Term() {
-//
-//	cout << "Term => Factor { (*|/) Factor } " << endl;
-//}
-//
-//void Factor() {
-//	
-//	cout << "Factor => Ident | IntConst | RealConst | StrConst | ( Expr ) | FuncCall ";
-//}
-//void FuncCall() {
-//	cout << "Ident ( [ArgList] ) " << endl;
-//}
-//
-//void ArgList() {
-//
-//	cout << "Expr { ,Expr} " << endl;
-//}
+	Type();
+	VarList();
+
+	expect(";");
+	cout << "Decl => Type VarList ;" << endl;
+}
+
+void FuncList() {
+
+	do {
+		Func();
+	} while (curToken.lexeme == "function");
+
+	cout << "FuncList =>Func {Func}" << endl;
+}
+void Func() {
+	expect("function");
+	Ident();
+
+	expect("(");
+
+	if (curToken.lexeme != ")") {
+		ParamList();
+	}
+
+	expect(")");
+
+	expect(":");
+	Type();
+	expect(";");
+
+	if (curToken.lexeme != "begin") {
+		DeclList();
+	}
+	expect("begin");
+	if (curToken.lexeme != "end") {
+		StmtList();
+
+	}
+	expect("end");
+
+	cout << "Func => function Ident ( [ParamList] ): Type ; [DeclList] begin [StmtList] end " << endl;
+}
+void ParamList() {
+
+
+	cout << "ParamList => Param {, Param}" << endl;
+}
+
+void Param() {
+
+	cout << "Param => Type Ident" << endl;
+}
+void Type() {
+
+	cout << "Type => int | real | string" << endl;
+}
+
+void VarList() {
+
+	cout << "VarList => Ident {,Ident}" << endl;
+}
+
+void StmtList() {
+
+	cout << "StmtList => Stmt{Stmt}" << endl;
+}
+
+void Stmt() {
+
+	cout << "Stmt => Assign | Read | Write | If | While | Do | Return" << endl;
+}
+void Assign() {
+
+	cout << "Assign => Ident := Expr;";
+}
+void Read() {
+
+	cout << "Read => read ( VarList ) ; " << endl;
+}
+
+void Write() {
+
+
+	cout << "Write => write ( Expr {, Expr} ) ;" << endl;
+
+}
+
+void If() {
+
+	cout << "If => if ( Cond ) begin StmtList end { elsif ( Cond ) begin StmtList end } [else begin StmtList end ]" << endl;
+}
+void While() {
+
+
+
+	cout << "While => while ( Cond ) begin [StmtList] end" << endl;
+}
+
+void Do() {
+	cout << "do => do [StmtList] until ( Cond ) ;" << endl;
+}
+
+void Return() {
+
+	cout << "Return => return Expr ;" << endl;
+}
+
+void Cond() {
+	cout << "Cond => Expr RelOp Expr" << endl;
+}
+void RelOp() {
+	cout << "RelOp => > | < | >= | <= | = | <> " << endl;
+}
+void Expr() {
+	cout << "Expr => Term { (+|-) Term }" << endl;
+}
+void Term() {
+
+	cout << "Term => Factor { (*|/) Factor } " << endl;
+}
+
+void Factor() {
+	
+	cout << "Factor => Ident | IntConst | RealConst | StrConst | ( Expr ) | FuncCall ";
+}
+void FuncCall() {
+	cout << "Ident ( [ArgList] ) " << endl;
+}
+
+void ArgList() {
+
+	cout << "Expr { ,Expr} " << endl;
+}
 
 
 
